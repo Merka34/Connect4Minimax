ca684340da3e7aed99089a3cea96cada83694409
Violeta <Violeta@DESKTOP-1ITE4QM>
2022-11-05 17:22:27 -0600

    Agregar archivos de proyecto.

---
+++ Connect4Minimax/Connect4VM.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4Minimax
+{
+    public class Connect4VM : INotifyPropertyChanged
+    {
+        public static Connect4VM? Instance;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _message = "Turno del Jugador";
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message))); }
+        }
+
+
+        private ObservableCollection<int> _coloresFila1;
+
+        public ObservableCollection<int> ColoresFila1
+        {
+            get { return _coloresFila1; }
+            set { _coloresFila1 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila1))); }
+        }
+
+        private ObservableCollection<int> _coloresFila2;
+
+        public ObservableCollection<int> ColoresFila2
+        {
+            get { return _coloresFila2; }
+            set { _coloresFila2 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila2))); }
+        }
+
+        private ObservableCollection<int> _controlesFila3;
+
+        public ObservableCollection<int> ColoresFila3
+        {
+            get { return _controlesFila3; }
+            set { _controlesFila3 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila3))); }
+        }
+
+        private ObservableCollection<int> _controlesFila4;
+
+        public ObservableCollection<int> ColoresFila4
+        {
+            get { return _controlesFila4; }
+            set { _controlesFila4 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila4))); }
+        }
+
+        private ObservableCollection<int> _controlesFila5;
+
+        public ObservableCollection<int> ColoresFila5
+        {
+            get { return _controlesFila5; }
+            set { _controlesFila5 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila5))); }
+        }
+
+        private ObservableCollection<int> _controlesFila6;
+
+        public ObservableCollection<int> ColoresFila6
+        {
+            get { return _controlesFila6; }
+            set { _controlesFila6 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila6))); }
+        }
+
+        private ObservableCollection<int> _controlesFila7;
+
+        public ObservableCollection<int> ColoresFila7
+        {
+            get { return _controlesFila7; }
+            set { _controlesFila7 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColoresFila7))); }
+        }
+
+        public Connect4VM()
+        {
+            Instance = this;
+            ColoresFila1 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila2 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila3 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila4 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila5 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila6 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+            ColoresFila7 = new ObservableCollection<int> { 0, 0, 0, 0, 0, 0 };
+        }
+    }
+}
---
+++ Connect4Minimax/MainWindow.xaml.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Connect4Minimax
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public int cx = 15;
+        public int cy = 70;
+        public int Level = 3;
+        public Conecta4 conecta4 = new Conecta4();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Registrar()
+        {
+            int q = 0;
+            int w = 5;
+
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    if (conecta4.arr[q, w] == 0)
+                    {
+                        switch (q)
+                        {
+                            case 0:
+                                Connect4VM.Instance.ColoresFila1[w] = 0;
+                                break;
+                            case 1:
+                                Connect4VM.Instance.ColoresFila2[w] = 0;
+                                break;
+                            case 2:
+                                Connect4VM.Instance.ColoresFila3[w] = 0;
+                                break;
+                            case 3:
+                                Connect4VM.Instance.ColoresFila4[w] = 0;
+                                break;
+                            case 4:
+                                Connect4VM.Instance.ColoresFila5[w] = 0;
+                                break;
+                            case 5:
+                                Connect4VM.Instance.ColoresFila6[w] = 0;
+                                break;
+                            case 6:
+                                Connect4VM.Instance.ColoresFila7[w] = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    if (conecta4.arr[q, w] == 1)
+                    {
+                        switch (q)
+                        {
+                            case 0:
+                                Connect4VM.Instance.ColoresFila1[w] = 1;
+                                break;
+                            case 1:
+                                Connect4VM.Instance.ColoresFila2[w] = 1;
+                                break;
+                            case 2:
+                                Connect4VM.Instance.ColoresFila3[w] = 1;
+                                break;
+                            case 3:
+                                Connect4VM.Instance.ColoresFila4[w] = 1;
+                                break;
+                            case 4:
+                                Connect4VM.Instance.ColoresFila5[w] = 1;
+                                break;
+                            case 5:
+                                Connect4VM.Instance.ColoresFila6[w] = 1;
+                                break;
+                            case 6:
+                                Connect4VM.Instance.ColoresFila7[w] = 1;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    if (conecta4.arr[q, w] == 2)
+                    {
+                        switch (q)
+                        {
+                            case 0:
+                                Connect4VM.Instance.ColoresFila1[w] = 2;
+                                break;
+                            case 1:
+                                Connect4VM.Instance.ColoresFila2[w] = 2;
+                                break;
+                            case 2:
+                                Connect4VM.Instance.ColoresFila3[w] = 2;
+                                break;
+                            case 3:
+                                Connect4VM.Instance.ColoresFila4[w] = 2;
+                                break;
+                            case 4:
+                                Connect4VM.Instance.ColoresFila5[w] = 2;
+                                break;
+                            case 5:
+                                Connect4VM.Instance.ColoresFila6[w] = 2;
+                                break;
+                            case 6:
+                                Connect4VM.Instance.ColoresFila7[w] = 2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    q++;
+                }
+                q = 0;
+                w--;
+            }
+        }
+
+
+        public async void PlayerClick(int idFila)
+        {
+            if (conecta4.endt == 0 && conecta4.turn != conecta4.cpu)
+            {
+                if ((idFila >= 0) && (idFila < 7))
+                {
+                    if (conecta4.add(idFila, conecta4.plr) == 0)
+                    {
+                        //Draw(this.CreateGraphics());
+                        Registrar();
+                        conecta4.turncheck();
+                        conecta4.turn = conecta4.cpu;
+                        Connect4VM.Instance.Message = "Turno del CPU";
+                        if (conecta4.endt == 1)
+                        {
+                            return;
+                        }
+                        await Task.Delay(800);
+                        if (conecta4.endt == 0)
+                        {
+                            int ck;
+                            conecta4.col = conecta4.Think();
+                            ck = conecta4.add(conecta4.col, conecta4.cpu);
+                            if (ck == 0)
+                            {
+                                conecta4.lin = 6 - conecta4.tops[conecta4.col];
+                                conecta4.turn = conecta4.plr;
+                            }
+                        }
+                        Registrar();
+                        //Draw(this.CreateGraphics());
+                        conecta4.turncheck();
+                        if (conecta4.endt == 1)
+                        {
+                            return;
+                        }
+                        Connect4VM.Instance.Message = "Turno del Jugador";
+                    }
+
+                }
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(1);
+        }
+
+        private void btnFila3_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(2);
+        }
+
+        private void btnFila4_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(3);
+        }
+
+        private void btnFila5_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(4);
+        }
+
+        private void btnFila6_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(5);
+        }
+
+        private void btnFila7_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerClick(6);
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            conecta4 = new Conecta4();
+            Registrar();
+            Connect4VM.Instance.Message = "Turno del Jugador";
+        }
+    }
+}
